d671760ae2e77a1f59a624df7cda112cd0631975
osefatul <sefaullahomar2@gmail.com>
2021-08-02 18:36:47 -0700

    image capturing first test succeded

--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -1,4 +1,5 @@
-import React, { useRef } from "react";
+import { RadioButtonChecked, RadioButtonUnchecked } from "@material-ui/icons";
+import React, { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 
 const videoConstraints = {
@@ -9,6 +10,16 @@ const videoConstraints = {
 
 function WebcamCapture() {
   const webcamRef = useRef(null);
+  const [image, setImage] = useState(null);
+
+  //it would run the function once, and then it would save the outputs and then when it happens to run again then it would know what is going to happen wihtout the calculation again.
+  //and it should know when to re-run from the dependancy changes. the capture will be faster than first, second, third ... times.
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+    setImage(imageSrc);
+  }, [webcamRef]);
   return (
     <div className="webcamCapture">
       <Webcam
@@ -19,6 +30,14 @@ function WebcamCapture() {
         width={videoConstraints.width}
         videoConstraints={videoConstraints}
       />
+
+      <RadioButtonUnchecked
+        className="webcamCapture_button"
+        onClick={capture}
+        fontSize="large"
+      />
+
+      <img src={image} alt="" />
     </div>
   );
 }
