d3e0108657af5ddfc1dcd3ca061d502a7f031e6c
osefatul <sefaullahomar2@gmail.com>
2021-08-03 18:38:03 -0700

    preview design is done

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,13 +1,26 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { selectCameraImage } from "../features/cameraSlice";
+import { resetCameraImage, selectCameraImage } from "../features/cameraSlice";
 import "./Preview.css";
+import Close from "@material-ui/icons/Close";
+
+import {
+  TextFields,
+  AttachFile,
+  Create,
+  Crop,
+  MusicNote,
+  Note,
+  Timer,
+  Send,
+} from "@material-ui/icons";
 
 function Preview() {
   //pull the cameraImage from the redux store
   const cameraImage = useSelector(selectCameraImage);
   const history = useHistory();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!cameraImage) {
@@ -15,10 +28,29 @@ function Preview() {
     }
   }, [cameraImage, history]);
 
+  const closePreview = () => {
+    //this will assign the cameraImage back to null and then when there is no image the useffect will trigger because of the condition.
+    dispatch(resetCameraImage());
+  };
+
   return (
     <div className="preview">
-      <h2>THIS IS YOUR PREVIEW</h2>
+      <Close className="preview_close" onClick={closePreview} />
+      <div className="preview_toolbarRight">
+        <TextFields />
+        <Create />
+        <Note />
+        <MusicNote />
+        <AttachFile />
+        <Crop />
+        <Timer />
+      </div>
       <img src={cameraImage} alt="" />
+
+      <div className="preview_footer">
+        <h2>Send Now</h2>
+        <Send fontSize="small" className="preview_sendIcon" />
+      </div>
     </div>
   );
 }
