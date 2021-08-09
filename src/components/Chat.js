ebddb3a64f9dc84094422b1040936f69cad50caa
osefatul <sefaullahomar2@gmail.com>
2021-08-08 19:24:18 -0700

    installed and used ReactTimeago library

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,15 +1,22 @@
 import { Avatar } from "@material-ui/core";
+import { StopRounded } from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
+import ReactTimeago from "react-timeago";
 
 function Chat({ id, username, timestamp, profilePic, imageUrl, read }) {
   return (
     <div className="chat">
-      <Avatar srcc={profilePic} />
+      <Avatar src={profilePic} className="chat_avatar" />
       <div className="chat_info">
         <h4>{username}</h4>
-        <p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
+        <p>
+          Tap to view -{" "}
+          <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} />{" "}
+        </p>
       </div>
+
+      {!read && <StopRounded className="chat_readIcon" />}
     </div>
   );
 }
