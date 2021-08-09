dd18ced18b36011595db1d50aeb47bdd89a1bfdb
osefatul <sefaullahomar2@gmail.com>
2021-08-08 23:21:49 -0700

    viewdone

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectSelectedImage } from "../features/appSlice";
 import "./ChatView.css";
 import { useHistory } from "react-router";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 function ChatView() {
   const selectedImage = useSelector(selectSelectedImage);
@@ -22,6 +23,19 @@ function ChatView() {
   return (
     <div className="chatView">
       <img src={selectedImage} onClick={exit} />
+      <CountdownCircleTimer
+        isPlaying
+        duration={10}
+        strokeWidth={6}
+        size={50}
+        colors={[
+          ["004777", 0.33],
+          ["F7B801", 0.33],
+          ["A30000", 0.33],
+        ]}
+      >
+        {" "}
+      </CountdownCircleTimer>
     </div>
   );
 }
